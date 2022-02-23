8dd5519f84981fb2900f2c3ee05c28e4230c5c2a
Sayaliparchande <95363550+Sayaliparchande@users.noreply.github.com>
2022-02-23 16:34:44 +0530

    Add project files.

---
+++ Burgerking/Controllers/ProductController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Burgerking.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
